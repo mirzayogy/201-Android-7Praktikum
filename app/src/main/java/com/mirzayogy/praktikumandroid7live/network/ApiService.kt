3286af5ec5d8326c4df6e60b619daa68c7410e0a
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2021-03-13 21:18:54 +0800

    Post data

--- app/src/main/java/com/mirzayogy/praktikumandroid7live/network/ApiService.kt
+++ app/src/main/java/com/mirzayogy/praktikumandroid7live/network/ApiService.kt
@@ -1,12 +1,17 @@
 package com.mirzayogy.praktikumandroid7live.network
 
 import com.mirzayogy.praktikumandroid7live.model.Jenisbarang
+import com.mirzayogy.praktikumandroid7live.model.JenisbarangData
+import com.mirzayogy.praktikumandroid7live.model.JenisbarangResponse
 import com.mirzayogy.praktikumandroid7live.model.User
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 //private const val BASE_URL = "https://reqres.in/api/"
 private const val BASE_URL = "http://10.0.2.2/praktikum-penjualan-api-starter/api/"
@@ -25,8 +30,11 @@ interface ApiService {
     @GET("users?page=1")
     suspend fun getUsers(): User
 
-    @GET("jenisbarang/read.php")
+    @GET("jenisbarang/read-free.php")
     suspend fun getJenisbarang(): Jenisbarang
+
+    @POST("jenisbarang/create.php")
+    suspend fun create(@Body jenisbarangData: JenisbarangData): Response<JenisbarangResponse>
 }
 
 object Api {
--- app/src/main/java/com/mirzayogy/praktikumandroid7live/ui/jenisbarang/JenisbarangViewModel.kt
+++ app/src/main/java/com/mirzayogy/praktikumandroid7live/ui/jenisbarang/JenisbarangViewModel.kt
@@ -5,11 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mirzayogy.praktikumandroid7live.model.Jenisbarang
+import com.mirzayogy.praktikumandroid7live.model.JenisbarangData
+import com.mirzayogy.praktikumandroid7live.model.JenisbarangResponse
 import com.mirzayogy.praktikumandroid7live.network.Api
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class JenisbarangViewModel : ViewModel() {
     private val _response = MutableLiveData<Jenisbarang>()
+    val createResponse = MutableLiveData<Response<JenisbarangResponse>>()
 
     val response: LiveData<Jenisbarang>
         get() = _response
@@ -26,6 +30,13 @@ class JenisbarangViewModel : ViewModel() {
             } catch (e: Exception) {
                 _response.value = null
             }
+        }
+    }
+
+    fun create(jenisbarangData: JenisbarangData) {
+        viewModelScope.launch {
+            val response = Api.retrofitService.create(jenisbarangData)
+            createResponse.value = response
         }
     }
 }
